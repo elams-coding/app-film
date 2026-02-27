6d03ea22e5349fadb58fe30c08edcccf15fa6733
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2026-02-27 21:56:38 +0100

    chore: add Reader class skeleton

---
+++ src/main/java/Reader.java
@@ -0,0 +1,27 @@
+
+import java.util.List;
+
+public class Reader {
+
+    private Reader() {
+    }
+
+    private static void initialisation() {
+        // TODO: display title
+        // TODO: display files
+    }
+
+    public static void start() {
+        // TODO: loop while app running
+        // TODO: execute what the user want
+    }
+
+    private static void chooseOption(List<String> options) {
+
+    }
+
+    private static void executeChoice() {
+
+    }
+
+}
